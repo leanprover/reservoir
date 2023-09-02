6e584d65599eca6faecc39d6df4f04073c18e98a
tydeu <tydeu@hatpress.net>
2023-09-01 21:23:35 -0400

    feat: try build, then update, then try again

--- build-one.sh
+++ build-one.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+TESTBED="${TESTBED:-testbed}"
+
 REPO=$1
-DIR=$2
+DIR="${2:-$TESTBED/repos/$REPO}"
 TOOLCHAIN=${3:-"leanprover/lean4:$(gh release list -R leanprover/lean4 -L 1 | cut -f1)"}
 
-mkdir -p $DIR
-rm -rf $DIR
-git clone https://github.com/$REPO $DIR
-cd $DIR
+mkdir -p "$DIR"
+rm -rf "$DIR"
+git clone https://github.com/$REPO "$DIR"
+cd "$DIR"
 echo -n "$TOOLCHAIN" > lean-toolchain
-lake update
 lake exe cache get || true
-lake build
+if ! lake build; then
+  echo "info: build failed, updating and trying again" >&2
+  lake clean
+  lake update
+  lake exe cache get || true
+  lake build
+fi
+echo "info: succesfully built $REPO on $TOOLCHAIN" >&2
--- build.sh
+++ build.sh
@@ -5,16 +5,10 @@ TESTBED="${TESTBED:-testbed}"
 REPO_FILE="${1:-"$TESTBED/test-repos.txt"}"
 TOOLCHAIN=${2:-"leanprover/lean4:$(gh release list -R leanprover/lean4 -L 1 | cut -f1)"}
 
-i=1
 while read -r REPO; do
-  DIR="$TESTBED/repos/$i"
-  set +e
+  DIR="$TESTBED/repos/$REPO"
   echo "info: building $REPO on $TOOLCHAIN in $DIR" >&2
-  ./build-one.sh $REPO "$DIR" $TOOLCHAIN >&2
-  RC=$?
-  set -e
-  if [ $RC = 0 ]; then
+  if ./build-one.sh $REPO "$DIR" $TOOLCHAIN >&2; then
     echo $REPO
   fi
-  ((i++))
 done < "$REPO_FILE"
