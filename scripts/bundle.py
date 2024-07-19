6b9511234a9d460b2b56bd3def8c89246a99d76f
Mac Malone <tydeu@hatpress.net>
2024-07-18 21:00:30 -0400

    refactor: package toolchain list
    * Toolchains are now sorted first by minor version and then by date.
    * Package toolchain builds are initially limited to the latest 10
    toolchains with a "+ more" toggle to show the full list.

--- scripts/bundle.py
+++ scripts/bundle.py
@@ -21,10 +21,9 @@ if __name__ == "__main__":
   configure_logging(args.verbosity)
 
   toolchains = query_toolchains()
-  toolchainDates = dict((t['name'], of_utc_iso(t['date'])) for t in toolchains)
-  min_utc = datetime.min.replace(tzinfo=timezone.utc)
-  def toolchain_date(build: Build) -> datetime:
-    return toolchainDates.get(build['toolchain'], min_utc)
+  toolchain_sort_keys = dict((t['name'], toolchain_sort_key(t)) for t in toolchains)
+  def build_sort_key(build: Build):
+    return toolchain_sort_keys.get(build['toolchain'], MIN_TOOLCHAIN_SORT_KEY)
 
   pkgs, aliases = load_index(args.index, include_builds=True)
   pkgMap = dict((pkg['fullName'], pkg) for pkg in pkgs)
@@ -35,7 +34,7 @@ if __name__ == "__main__":
       pkg = pkgMap[full_name]
       pkg['builds'] = insert_build_results(pkg['builds'], pkg_results)
   for pkg in pkgMap.values():
-    pkg['builds'] = sorted(pkg['builds'], key=toolchain_date, reverse=True)
+    pkg['builds'] = sorted(pkg['builds'], key=build_sort_key, reverse=True)
 
   data = {
     'bundledAt': utc_iso_now(),
